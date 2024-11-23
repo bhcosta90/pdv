8d2c0a343ea058ab6305cd23adf77d9d7430ad3b
Bruno Costa <bhcosta90@gmail.com>
2024-11-23 15:38:52 -0300

    --wip-- [skip ci]

---
+++ .lefthook/pre-push/migrate.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+# Check if sail is enabled
+sailEnabled=$(grep -i '^SAIL_ENABLED' .env | cut -d '=' -f2 | tr '[:upper:]' '[:lower:]')
+sailCommand=$([[ "$sailEnabled" = "true" ]] && echo "./vendor/bin/sail" || echo "")
+echo "Running Tests"
+if [ -n "$sailCommand" ]; then
+    $sailCommand php artisan migrate
+    $sailCommand php artisan migrate:rollback
+else
+    php artisan migrate
+    php artisan migrate:rollback
+fi
+STATUS_CODE=$?
+if [ $STATUS_CODE -ne 0 ]; then
+    echo "Migration failed"
+    exit 1
+else
+    echo "Migration passed"
+fi
